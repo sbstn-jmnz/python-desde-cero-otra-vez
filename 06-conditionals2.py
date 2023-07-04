fcf044a01329a70e6a474d87d8c8bede7deffeaf
sebastian <sebastian@boolean.cl>
2023-07-03 22:08:45 -0400

    session 2

---
+++ 06-conditionals2.py
@@ -0,0 +1,8 @@
+# Las expresiones booleanas nos permiten hacer flujos de ejecución condicional utilizando estructuras de control.
+
+age = int(input("¿Cuál es tu edad?: "))
+
+if(age >= 18):
+  print("Eres mayor de edad")
+else:
+  print("Entonces eres menor de edad")
